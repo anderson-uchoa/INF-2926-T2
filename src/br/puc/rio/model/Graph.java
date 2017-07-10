6dfd090172b6d6bcc080f26f297c59d6955724e0
flavio-barros <juniorbarros3@gmail.com>
2017-07-10 12:14:01 -0300

    partial implementation branch-and-bound

--- src/br/puc/rio/model/Graph.java
+++ src/br/puc/rio/model/Graph.java
@@ -7,30 +7,21 @@ public class Graph {
 	public int quantityNodes = 0;
 	public Integer matrixAdj[][];
 	
-	
-	
 	public Graph(int quantityNodes) {
-		
-		this.quantityNodes = quantityNodes;
-		this.matrixAdj = new Integer[quantityNodes][quantityNodes];
+		this.quantityNodes = quantityNodes+1;
+		this.matrixAdj = new Integer[this.quantityNodes][this.quantityNodes];
 	}
 
-
 	public void createNodes(List<Integer> weights) {
-		// TODO Auto-generated method stub
-		
 		int count = 0;
 		for(int i = 0; i< quantityNodes; i++){
-			
 			for (int j = 0; j < quantityNodes; j++) {
-				
 				if (count < weights.size()) {
 					matrixAdj[i][j] = weights.get(count);
 					count++;
 				}
 			}
 		}
-		
 	}
 
 	
